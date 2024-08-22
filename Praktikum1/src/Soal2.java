0f5fb0c05b0673ef1b78d9d4d96ea67b5f7249a4
Dhira Ramadini <dhiraramadini@gmail.com>
2024-08-22 19:59:40 +0700

    Add files via upload

---
+++ Praktikum1/src/Soal2.java
@@ -0,0 +1,12 @@
+/*
+* Praktikum 1 ~ PBO PR
+* @dirarmdn 22/08/24
+* */
+
+public class Soal2 {
+    public static void main(String[] args) {
+        int i = 42;
+        String s = (i < 40)? "life" : (i > 50)? "universe" : "everything";
+        System.out.println(s);
+    }
+}
---
+++ Praktikum1/src/Welcome.java
@@ -0,0 +1,14 @@
+/*
+ * Praktikum 1 ~ PBO PR
+ * @dirarmdn 22/08/24
+ * */
+
+public class Welcome {
+    public static void main(String[] args) {
+        String greeting = "Welcome to Core Java!";
+        System.out.println(greeting);
+        for (int i = 0; i < greeting.length(); i++)
+            System.out.print("=");
+        System.out.println();
+    }
+}
